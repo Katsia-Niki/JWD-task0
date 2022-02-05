ac6009a30b312f9c0cb9c75054893eecbd40f593
Katsia-Niki <82093777+Katsia-Niki@users.noreply.github.com>
2022-02-05 15:59:30 +0300

    Add files via upload

--- service/impl/CalculateAverageImpl.java
+++ service/impl/CalculateAverageImpl.java
@@ -8,14 +8,14 @@ public class CalculateAverageImpl implements CalculateAverage{
 	@Override
 	public int calculateAverage(CustomArray custArr) {
 		
-		int sum = 0;
+		double sum = 0.0;
 		int[] array = custArr.getArray();
 		
 		for (int i : array) {
 			sum += i;
 		}
 		
-		int average = sum / array.length;
+		int average = (int) Math.round(sum / array.length);
 		
 		return average;
 	}
