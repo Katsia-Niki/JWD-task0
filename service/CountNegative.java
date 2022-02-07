df5e5dc3aeb0664f9e3bdad1bddab1d467674229
Katsia-Niki <82093777+Katsia-Niki@users.noreply.github.com>
2022-02-07 15:35:00 +0300

    Delete service directory

--- service/CountNegative.java
+++
@@ -1,9 +0,0 @@
-package by.jwd.pretask1.service;
-
-import by.jwd.pretask1.entity.CustomArray;
-
-public interface CountNegative {
-	
-	int countNegative(CustomArray custArr);
-
-}
--- service/FindMax.java
+++
@@ -1,9 +0,0 @@
-package by.jwd.pretask1.service;
-
-import by.jwd.pretask1.entity.CustomArray;
-
-public interface FindMax {
-	
-	int findMax(CustomArray custArr);
-
-}
