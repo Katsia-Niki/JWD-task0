9e3d36c2ecf43baf7c370bf8e2a286cdb858a419
Katsia-Niki <82093777+Katsia-Niki@users.noreply.github.com>
2022-02-16 23:47:21 +0300

    Delete observer directory

--- observer/CustomArrayObservable.java
+++
@@ -1,9 +0,0 @@
-package by.jwd.task0.observer;
-
-import by.jwd.task0.exception.CustomArrayException;
-
-public interface CustomArrayObservable {
-    void attach(CustomArrayObserver observer);
-    void detach(CustomArrayObserver observer);
-    void notifyObservers();
-}
--- observer/CustomArrayObserver.java
+++
@@ -1,7 +0,0 @@
-package by.jwd.task0.observer;
-
-import by.jwd.task0.exception.CustomArrayException;
-
-public interface CustomArrayObserver {
-    void changeArray(CustomArrayEvent event);
-}
