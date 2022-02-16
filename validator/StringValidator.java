2fc7c3e2a2f02d9693c96d90d046bc967d8d134e
Katsia-Niki <82093777+Katsia-Niki@users.noreply.github.com>
2022-02-16 23:45:42 +0300

    Delete validator directory

--- validator/StringValidator.java
+++
@@ -1,9 +0,0 @@
-package by.jwd.task0.validator;
-
-public interface StringValidator {
-	
-static final String STRING_AS_INT_ARRAY_REGEX = "^(-?\\d{1,10},\\s)+$";
-	
-	boolean validateString(String stringAsInt);
-
-}
