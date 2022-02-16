7f53eb4e0e1499817bdd61b0027a930c9d3e2f9e
Katsia-Niki <82093777+Katsia-Niki@users.noreply.github.com>
2022-02-16 23:46:47 +0300

    Delete creator directory

--- creator/CustomArrayCreator.java
+++
@@ -1,9 +0,0 @@
-package by.jwd.task0.creator;
-
-import by.jwd.task0.entity.CustomArray;
-
-public interface CustomArrayCreator {
-
-    CustomArray createCustomArray (int ... intArr);
-    CustomArray createFullCustomArray(int numberOfElements);
-}
